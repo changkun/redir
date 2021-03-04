4030b037893586418508a4519ef6b83207a83154
Changkun Ou <hi@changkun.de>
2021-03-04 11:58:18 +0100

    Revert "all: merge alias and count queries"
    This reverts commit c9d81c6508d61614a67a3c0a2ab8391bc76da0ae.
    
    Using lookup in aggregation is considered harmful and slow. See:
    https://jira.mongodb.org/browse/SERVER-41171

--- short.go
+++ short.go
@@ -278,8 +278,8 @@ type records struct {
 }
 
 type record struct {
-	Alias string `bson:"alias"`
-	URL   string `bson:"url"`
+	Alias string `bson:"_id"`
+	URL   string `bson:"-"`
 	UV    int64  `bson:"uv"`
 	PV    int64  `bson:"pv"`
 }
@@ -292,6 +292,7 @@ func (s *server) stats(ctx context.Context, kind aliasKind, w http.ResponseWrite
 	case kindRandom:
 		prefix = conf.R.Prefix
 	}
+
 	ars := records{
 		Title:           conf.Title,
 		Host:            r.Host,
@@ -303,6 +304,14 @@ func (s *server) stats(ctx context.Context, kind aliasKind, w http.ResponseWrite
 	if err != nil {
 		return err
 	}
+	as, err := s.db.Aliases(ctx, kind)
+	if err != nil {
+		return err
+	}
+	for idx := range rs {
+		rs[idx].URL = as[rs[idx].Alias]
+	}
 	ars.Records = rs
+
 	return statsTmpl.Execute(w, ars)
 }
